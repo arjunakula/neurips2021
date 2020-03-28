3fe81009e29eade67fbca053d63b5979fcb8b368
akula.arjun@gmail.com <akula.arjun@gmail.com>
2020-03-28 03:25:02 -0700

    first copy after adding attention to LSTM encoder

--- iep/models/layers.py
+++ iep/models/layers.py
@@ -57,6 +57,9 @@ class ResidualBlock_LangAttention(nn.Module):
     #self.lstm11 = nn.LSTM(q_dim, in_dim, bidirectional=False, batch_first=True)
     self.lstm22 = nn.GRU(in_dim*2, int(in_dim/2), bidirectional=True, batch_first=True)
 
+    self.attention_layer_q = Attention(in_dim)
+    self.attention_layer_tq = Attention(in_dim)
+
     #self.lang_tq = nn.Linear(tq_dim, in_dim)
     self.conv1 = nn.Conv2d(in_dim, out_dim, kernel_size=3, padding=1)
     self.conv11 = nn.Conv2d(in_dim, out_dim, kernel_size=3, padding=1)
@@ -77,13 +80,25 @@ class ResidualBlock_LangAttention(nn.Module):
     q = q.unsqueeze(0).cuda()
     t_q = t_q.unsqueeze(0).cuda() # to make t_q as 1Xseq_lenX300
 
+    #packed_embedded_q = nn.utils.rnn.pack_padded_sequence(q, [q.shape[1]],batch_first=True)
+    #packed_embedded_tq = nn.utils.rnn.pack_padded_sequence(t_q, [t_q.shape[1]],batch_first=True)
+
     tq_lstm, _ = self.lstm1(t_q)
     tq_lstm, _ = self.lstm2(tq_lstm) # output size of h_lstm is batchXseq_lenX300 ... so we need to
 
     q_lstm, _ = self.lstm11(q)
     q_lstm, _ = self.lstm22(q_lstm) # output size of h_lstm is batchXseq_lenX300 ... so we need to
 
-    txt_conv = self.conv11( F.relu(self.conv1(x) * q_lstm[:,-1].view(q.shape[0],-1,1,1)) ) * tq_lstm[:,-1].view(t_q.shape[0],-1,1,1)
+    q_attn = self.attention_layer_q(q_lstm, q.shape[1])
+    tq_attn = self.attention_layer_q(tq_lstm, t_q.shape[1])
+
+    #lang encode without attention
+    #txt_conv = self.conv11( F.relu(self.conv1(x) * q_lstm[:,-1].view(q.shape[0],-1,1,1)) ) * tq_lstm[:,-1].view(t_q.shape[0],-1,1,1)
+    
+    #lang encode with attention
+    txt_conv = self.conv11( F.relu(self.conv1(x) * q_attn.view(q.shape[0],-1,1,1)) ) * tq_attn.view(t_q.shape[0],-1,1,1)
+    
+
     #txt_conv = (self.conv1(x) * q_lstm[:,-1].view(q.shape[0],-1,1,1) ) * tq_lstm[:,-1].view(t_q.shape[0],-1,1,1)
     #txt_conv = (self.conv1(x) * q_lstm[:,-1].view(q.shape[0],-1,1,1) ) 
     #txt_conv = self.conv1(x)
@@ -110,3 +125,42 @@ class GlobalAveragePool(nn.Module):
 class Flatten(nn.Module):
   def forward(self, x):
     return x.view(x.size(0), -1)
+
+class Attention(nn.Module):
+    def __init__(self, feature_dim, step_dim=100, bias=True, **kwargs):
+        super(Attention, self).__init__(**kwargs)
+        
+        self.supports_masking = True
+
+        self.bias = bias
+        self.feature_dim = feature_dim
+        self.step_dim = step_dim
+        self.features_dim = 0
+        
+        weight = torch.zeros(feature_dim, 1)
+        nn.init.kaiming_uniform_(weight)
+        self.weight = nn.Parameter(weight)
+        
+    def forward(self, x, step_dim, mask=None):
+        self.step_dim = step_dim
+        feature_dim = self.feature_dim 
+        step_dim = self.step_dim
+
+        eij = torch.mm(
+            x.contiguous().view(-1, feature_dim), 
+            self.weight
+        ).view(-1, step_dim)
+        
+        if self.bias:
+            eij = eij + nn.Parameter(torch.zeros(step_dim)).cuda()
+            
+        eij = torch.tanh(eij)
+        a = torch.exp(eij)
+        
+        if mask is not None:
+            a = a * mask
+
+        a = a / (torch.sum(a, 1, keepdim=True) + 1e-10)
+
+        weighted_input = x * torch.unsqueeze(a, -1)
+        return torch.sum(weighted_input, 1)
