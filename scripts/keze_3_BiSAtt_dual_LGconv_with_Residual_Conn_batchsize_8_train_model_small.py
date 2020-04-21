b48a58a40bdd366bf04fb5086e1913631f653657
Arjun Akula <aakula@ucla.edu>
2020-04-21 00:36:56 -0700

    corrected checpoint name in keze3_...py file

--- scripts/keze_3_BiSAtt_dual_LGconv_with_Residual_Conn_batchsize_8_train_model_small.py
+++ scripts/keze_3_BiSAtt_dual_LGconv_with_Residual_Conn_batchsize_8_train_model_small.py
@@ -697,7 +697,7 @@ if __name__ == '__main__':
   args.model_type = "PG+EE"
   args.num_iterations = 500000
   args.learning_rate = 1e-4
-  args.checkpoint_path = "data/run_fixedPG+EE_ref_singleObject_small/keze_2_BiSAtt_single_LGConv_batchsize_8.pt"
+  args.checkpoint_path = "data/run_fixedPG+EE_ref_singleObject_small/keze_3_BiSAtt_dual_LGconv_with_Residual_Conn_8.pt"
   args.checkpoint_every = 10000
   args.train_refexp_h5 = "data/small_dataset/train_refexps.h5"
   args.train_features_h5 = "data/train_features.h5"
